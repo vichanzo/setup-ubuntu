f8cc0ede2764800dd1d3be6dbacb03a324eef843
vichanzo <75827403+vichanzo@users.noreply.github.com>
2020-12-10 22:41:59 -0800

    Add files via upload

---
+++ ubuntu-20_04-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ubuntu (xfce4) 20.04 setup script.
+
+
+# Initial Software
+sudo apt update
+sudo apt install --ignore-missing -y \
+gnome-todo \
+baobab \
+solaar \
+caffeine \
+htop \
+vino \
+shotwell \
+gnome-calendar \
+aisleriot \
+gnome-mines \
+gnome-sudoku \
+neofetch \
+remmina \
+remmina-common \
+remmina-plugin-rdp \
+remmina-plugin-secret \
+remmina-plugin-vnc
